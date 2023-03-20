868c216bef3a5059e2073ab5a97ac0d1007ad8cb
Pedro Ferri <pedro@cybersquare.com.br>
2023-03-20 11:10:36 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,29 +22,31 @@
 	  });
 	}
 	
-    $(".dropdown-menu a.dropdown-toggle").on("click", function (e) {
-        if (!$(this).next().hasClass("show")) {
-            $(this).parents(".dropdown-menu").first().find(".show").removeClass("show");
-        }
-        var $subMenu = $(this).next(".dropdown-menu");
-        $subMenu.toggleClass("show");
-        $(this)
-            .parents("li.nav-item.dropdown.show")
-            .on("hidden.bs.dropdown", function (e) {
-                $(".dropdown-submenu .show").removeClass("show");
-            });
-        return false;
-    });
-    if ($(".search-box-outer").length) {
-        $(".search-box-outer").on("click", function () {
-            $("body").addClass("search-active");
-        });
-        $(".close-search").on("click", function () {
-            $("body").removeClass("search-active");
-        });
-    }
 	
     $(window).on("load", function () {
+		
+		
+		$(".dropdown-menu a.dropdown-toggle").on("click", function (e) {
+			if (!$(this).next().hasClass("show")) {
+				$(this).parents(".dropdown-menu").first().find(".show").removeClass("show");
+			}
+			var $subMenu = $(this).next(".dropdown-menu");
+			$subMenu.toggleClass("show");
+			$(this)
+				.parents("li.nav-item.dropdown.show")
+				.on("hidden.bs.dropdown", function (e) {
+					$(".dropdown-submenu .show").removeClass("show");
+				});
+			return false;
+		});
+		if ($(".search-box-outer").length) {
+			$(".search-box-outer").on("click", function () {
+				$("body").addClass("search-active");
+			});
+			$(".close-search").on("click", function () {
+				$("body").removeClass("search-active");
+			});
+		}		
 		
 		$(".preloader").fadeOut("slow");
 		
