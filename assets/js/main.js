9c2c2cd45851add578fd891808ba9919b6123fe5
Pedro Ferri <pedro@cybersquare.com.br>
2023-12-14 16:33:16 -0300

    removido o mautic

--- assets/js/main.js
+++ assets/js/main.js
@@ -59,16 +59,6 @@
         });
 		
 		
-		if(document.location.protocol.includes("http")){
-			(function(w,d,t,u,n,a,m){w['MauticTrackingObject']=n;
-				w[n]=w[n]||function(){(w[n].q=w[n].q||[]).push(arguments)},a=d.createElement(t),
-				m=d.getElementsByTagName(t)[0];a.async=1;a.src=u;m.parentNode.insertBefore(a,m)
-			})(window,document,'script','https://m.dienimoraes.com.br/mtc.js','mt');
-
-			mt('send', 'pageview');
-		}
-		
-		
 		new WOW().init();
 		$(".category-slider").owlCarousel({
 			loop: true,
